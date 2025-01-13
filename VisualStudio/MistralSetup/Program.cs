c94f4a1629ccb6b00293181048ef50a958a44b4d
Elijah Kelting <elijahakbusiness@gmail.com>
2025-01-13 14:58:14 -0700

    mistral Setup

---
+++ VisualStudio/MistralSetup/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MistralSetup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
