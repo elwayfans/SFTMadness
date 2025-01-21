7803bd7b51a896462120faac83cde0ced8fc59b1
Elijah Kelting <elijahakbusiness@gmail.com>
2025-01-21 13:27:51 -0700

    save and load functionality for json works

--- Mistral/Mist.py
+++ Mistral/Mist.py
@@ -1,4 +1,5 @@
 import os
+import json
 from mistralai import Mistral
 
 #start of new implementation
@@ -16,6 +17,7 @@ class AIModelClass:
     bot_name = ""
     message_history = []
     
+    
     role_types = ['system', 'assistant', 'user']
     
     #TODO: make save message history function and load message history function
@@ -24,20 +26,8 @@ class AIModelClass:
     def __init__(self, college_name, bot_name):
         self.college_name = college_name
         self.bot_name = bot_name
-        #system prompts, these are what control the AI's rule set and what it should follow over what the user says
-        self.add_system_prompt(f"""You are a helpful assistant tasked with promoting {college_name}. 
-        Ensure all responses focus solely on {college_name}, its programs, values, achievements, and unique offerings. 
-        Avoid mentioning other institutions or making comparisons unless specifically asked to do so by the user.
-
-        try to keep things concise as possible, while still keeping the conversational/professional tone.
-
-        Ensure that responses to prospective student questions use varied sentence structures and tones to keep the conversation engaging. 
-        Avoid reusing exact phrasing from the initial email.
-        Prompt a few questions the user can ask you the assistant about the school. Questions like 'What degrees does neumont offer?'
-
-        Do not suggest questions that have either already been answered or have been asked before.""")
         
-        self.training_data()
+        self.load_message_history('sample.json')
         
     def create_message(self, role: str, message: str):
         return {"role": role, "content":message}
@@ -70,9 +60,19 @@ class AIModelClass:
     def training_data(self):
         self.add_user_prompt("What scholarships do you offer?")
         self.add_assistant_prompt(f"""Great question!
-                        {college_name} offers a variety of scholarships, including merit-based awards for academic excellence, need-based assistance, and special grants for extracurricular achievements. 
-                        Our admissions team is happy to guide you through the application process.
-                        Let me know if you'd like more details on specific opportunities!""")
+{college_name} offers a variety of scholarships, including merit-based awards for academic excellence, need-based assistance, and special grants for extracurricular achievements. 
+Our admissions team is happy to guide you through the application process.
+Let me know if you'd like more details on specific opportunities!""")
+        
+    def save_message_history(self):
+        json_object = json.dumps(self.message_history, indent=4)
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+    
+    def load_message_history(self, json_file_name):
+        with open(json_file_name, 'r') as openfile:
+            json_object = json.load(openfile)
+            print(json_object)
     
     
 if __name__ == "__main__":
@@ -82,9 +82,9 @@ if __name__ == "__main__":
     AIModel = AIModelClass(college_name, bot_name)
 
     AIModel.add_user_prompt(f"""Write a casual and inviting email to promote {college_name} to prospective students and their families. 
-                    Use a friendly and personal tone. Highlight the school's unique features, such as academic programs, campus life, and opportunities for growth, in a concise manner. 
-                    Mention that you are an AI assistant and encourage the recipient to reply with any questions they might have, offering examples of questions they can ask. 
-                    Keep the email short and engaging.""")
+Use a friendly and personal tone. Highlight the school's unique features, such as academic programs, campus life, and opportunities for growth, in a concise manner. 
+Mention that you are an AI assistant and encourage the recipient to reply with any questions they might have, offering examples of questions they can ask. 
+Keep the email short and engaging.""")
 
     #prints original email
     chat_response = client.chat.complete(
