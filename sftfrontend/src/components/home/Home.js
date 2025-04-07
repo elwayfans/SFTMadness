f81d81d6837082bd6e838f9e774b2a7fac87f05a
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-04-07 16:49:54 -0600

    home.js

--- sftfrontend/src/components/home/Home.js
+++ sftfrontend/src/components/home/Home.js
@@ -5,12 +5,36 @@
 
 ✖add information box at bottom
 */
-
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>Home</div>
+        <div className='homeBody'>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae
+                scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices
+                nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut
+                aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac.
+                In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae
+                dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum.
+            </p>
+            <div>
+                <button onClick={() => navigate('/login')}>Login</button>
+                <button onClick={() => navigate('/register')} >Register</button>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae
+                scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices
+                nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut
+                aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac.
+                In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae
+                dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum.
+            </p>
+        </div>
     )
 }
 
