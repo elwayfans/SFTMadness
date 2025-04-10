649e0e1af1faf5f932b354ba3cfca4c95c55fe67
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-04-10 13:50:33 -0600

    comment in the navbar
    just making a comment on what will be changed when a user is signed in

--- sftfrontend/src/components/navbar/NavBar.js
+++ sftfrontend/src/components/navbar/NavBar.js
@@ -11,6 +11,22 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
+    /*
+    if logged in display the following navbar and have the profile link show a drop down menu that shows settings and sign out
+
+    <nav>
+      <img src="/images/sft.png" alt="sft" className="images" />
+      <ul>
+        <li>
+          <Link className='flex-container' to="/">Home</Link>
+          <Link className='flex-container' to="/aboutus">About Us</Link>
+          <Link className='flex-container' to="/ai-chatbot">Chat(AI)</Link>
+          <Link className='flex-container' to="/Profile">Profile</Link>
+                make the above link a drop down that has Settings and Sign Out
+        </li>
+      </ul>
+    </nav>
+    */
     <nav>
       <img src="/images/sft.png" alt="sft" className="images" />
       <ul>
