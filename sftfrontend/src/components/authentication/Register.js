7d5c6105798e4934c0476d8f357b7e8e48d519ac
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-04-18 16:42:33 -0600

    updating response

--- sftfrontend/src/components/authentication/Register.js
+++ sftfrontend/src/components/authentication/Register.js
@@ -58,7 +58,7 @@ const Register = () => {
     } else {
       setErrors({});
       try {
-        const response = await fetch("http://localhost:3001/register", {
+        const response = await fetch("http://localhost:3001/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
