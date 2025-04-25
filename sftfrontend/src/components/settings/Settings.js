c9d62bb19dcebfb05214ade376e038086ebcd351
floralhaven <141456665+floralhaven@users.noreply.github.com>
2025-04-25 13:11:54 -0600

    Start of AI
    Has the new Ai Customization section.

--- sftfrontend/src/components/settings/Settings.js
+++ sftfrontend/src/components/settings/Settings.js
@@ -5,10 +5,12 @@ Hook up the update password with the backend
 Hook up the 2FA with the backend
 */
 import React, {useState, useEffect} from 'react'
-import './Settings.css';
+import { useNavigate } from "react-router-dom"
+import './Settings.css'
 
 const Settings = () => {
     const [darkMode, setDarkMode] = useState(false);
+    const navigate = useNavigate();
 
     // Toggle class on body for global dark/light mode
     useEffect(() => {
@@ -36,19 +38,8 @@ const Settings = () => {
                     <label>Light</label>
                 </div>
 
-                <hr />
-
-                <div className="password-section">
-                    <label htmlfpr="password">Current Password:</label>
-                    <input type="password" id="password" required/>
-                    <label htmlFor="password">Updated Password:</label>
-                    <input type="password" id="password" required/>
-                    <button className="confirm-button">Confirm</button>
-                </div>
-
-                <hr />
-
                 <button className="twofa-button">2 Factor Authentication</button>
+                <button className="password-button"onClick={() => navigate('/login')}>Forgot Password</button>
             </div>
         </div>
     );
