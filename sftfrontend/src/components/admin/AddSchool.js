512def40d5c7c9d66b19375eb357f0c9659a2979
floralhaven <141456665+floralhaven@users.noreply.github.com>
2025-05-19 15:57:16 -0600

    filled out the add school file

--- sftfrontend/src/components/admin/AddSchool.js
+++ sftfrontend/src/components/admin/AddSchool.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './Admin.css'; // Reuse styles defined in your Admin.css
+import './Admin.css'; 
 
 export const AddSchool = () => {
     const [form, setForm] = useState({
